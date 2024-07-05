def3491791234f9422e72b5d68cc002fa42ea40f
heejin55 <167740922+heejin55@users.noreply.github.com>
2024-07-05 13:10:19 +0900

    .
    .

---
+++ 90_test/자바스크립트(Javascript)_이희진/script/script.js
@@ -0,0 +1,9 @@
+// 요소찾기
+const tab1 = document.querySelector('#tabs li>a');
+const tab2 = document.querySelector('#tabs li>a');
+const tab3 = document.querySelector('#tabs li>a');
+const tab4 = document.querySelector('#tabs li>a');
+const tab5 = document.querySelector('#tabs li>a');
+// console.log(tab1,tab2,tab3,tab4,tab5);
+
+// 이벤트 처리
