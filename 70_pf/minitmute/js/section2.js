f8133284b05b3874d6aa40e1635fae9270a82533
heejin55 <167740922+heejin55@users.noreply.github.com>
2024-07-05 09:55:15 +0900

    .
    .

--- 70_pf/minitmute/js/section2.js
+++ 70_pf/minitmute/js/section2.js
@@ -1,11 +1,15 @@
 // 요소 찾기
 const btn1 = document.querySelectorAll('#tabs a').item(0);
 const btn2 = document.querySelectorAll('#tabs a').item(1);
+const btn3 = document.querySelectorAll('#tabs a').item(2);
+const btn4 = document.querySelectorAll('#tabs a').item(3);
+console.log(btn1,btn2,btn3,btn4);
+
 const cont1 = document.querySelectorAll('#tabs>div')[0];
 const cont2 = document.querySelectorAll('#tabs>div')[1];
 
 
-
+// 이벤트 처리
 btn1.onclick = function(){
     cont1.classList.add('on');
     cont2.classList.remove('on');
