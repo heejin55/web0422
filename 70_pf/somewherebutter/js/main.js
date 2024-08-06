3e885434976afe43d759c38b9317e63e40b9ee74
heejin55 <sandmute@naver.com>
2024-08-06 16:31:30 +0900

    ㅇ
    ㅇ

--- 70_pf/somewherebutter/js/main.js
+++ 70_pf/somewherebutter/js/main.js
@@ -1,7 +1,9 @@
 
-$('.bxslider').bxSlider({
-    minSlides: 3,
-    maxSlides: 4,
-    slideWidth: 244,
-    slideMargin: 10
-  });
+$(()=>{
+    $('.bxslider').bxSlider({
+        minSlides: 4,
+        maxSlides: 5,
+        slideWidth: 244,
+        slideMargin: 10
+      });
+});
