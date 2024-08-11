4d640e4693bc9b57d8ae380a35d2775978d2f960
heejin55 <sandmute@naver.com>
2024-08-11 23:06:14 +0900

    썸웨어
    ㅇ

--- 70_pf/somewherebutter/js/main.js
+++ 70_pf/somewherebutter/js/main.js
@@ -1,11 +1,29 @@
 
-$(()=>{
-    $('.new_butter_item').bxSlider({
-        minSlides: 5,
-        maxSlides: 7,
-        slideWidth: 243,
-        slideMargin: 11,
-        moveSlides: 1,
-        pager: false
-      });
+$(() => {
+  $('.new_butter_item').bxSlider({
+    minSlides: 5,
+    maxSlides: 7,
+    slideWidth: 243,
+    slideMargin: 11,
+    moveSlides: 1,
+    pager: false
+  });
+
+  // 햄버거버튼
+  const nav = document.querySelector('nav');
+  const hamburger = document.querySelector('.hamburger');
+
+  //이벤트 처리~
+  hamburger.onclick = () => {
+    btnMotion(event.currentTarget) // 함수호출~
+  };
+  function btnMotion(x) {
+    // 햄버거 버튼: 트리플바 -> x로 바뀜
+    // 또 누르면 x -> 트리플바
+    x.classList.toggle("change");
+
+    // nav 태그에 change가 추가
+    nav.classList.toggle("change");
+
+  }
 });
