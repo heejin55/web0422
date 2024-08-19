3772e9a981f9228c71651e95c7ffec1f3606da9a
heejin55 <sandmute@naver.com>
2024-08-19 14:24:12 +0900

    f
    f

--- 70_pf/team2/team_3/js/sub/advice.js
+++ 70_pf/team2/team_3/js/sub/advice.js
@@ -1,5 +1,23 @@
 $(() => {
-    // 섹션5 캐러셀
+  //섹션4 탭
+  const btn1 = $('#ad_s4 li').eq(0);
+  const btn2 = $('#ad_s4 li').eq(1);
+  const cont1 = $('#ad_s4 .tabs>div').eq(0);
+  const cont2 = $('#ad_s4 .tabs>div').eq(1);
+
+  btn1.clik(() => {
+    cont1.css({display: 'block'});
+    cont2.css({display: 'none'});
+  });
+
+  btn2.clik(() => {
+    cont2.css({display: 'block'});
+    cont1.css({display: 'none'});
+  });
+
+  
+  
+  // 섹션5 캐러셀
     $('.ad_slide').slick({
         infinite: true,
         slidesToShow: 3,
