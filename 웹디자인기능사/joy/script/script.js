ea19c914e94685213138d83c6f6a0ff54f5588ba
heejin55 <sandmute@naver.com>
2024-08-22 00:01:18 +0900

    t
    t

--- 웹디자인기능사/joy/script/script.js
+++ 웹디자인기능사/joy/script/script.js
@@ -6,6 +6,21 @@
     
 
     // 탭
+    const btn1 = $('.tabs>li').eq(0);
+    const btn2 = $('.tabs>li').eq(1);
+
+    const tab1 = $('#tab1>table');
+    const tab2 = $('#tab2>ul');
+    
+
+    btn1.click(() => {
+        tab1.css({display: 'block'});
+        tab2.css({display: 'none'});
+    });
+    btn2.click(() => {
+        tab2.css({display: 'block'});
+        tab1.css({display: 'none'});
+    });
 
 
     //모달팝업
