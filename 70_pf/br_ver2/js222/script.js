c28db932bac0a126888cf8b3e2b295f410cbd48a
heejin55 <sandmute@naver.com>
2024-08-22 16:02:20 +0900

    .
    .

--- 70_pf/br_ver2/js222/script.js
+++ 70_pf/br_ver2/js222/script.js
@@ -1,18 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    // 모바일 햄버거 버튼 모양 모션 처리와 헤더 등장 모션
+    // 햄버거 버튼 스크립트, 헤더 등장 모션
 
-    // 요소 찾기
-    const nav = document.querySelector("nav");
-    const collapsedMenuIcon = document.querySelector(".collapsedMenuIcon");
+    // 요소찾기~
+    const nav = document.querySelector('nav');
+    const collapsedMenuIcon = document.querySelector('.collapsedMenuIcon');
 
-    // 이벤트 처리
+    //이벤트 처리~
     collapsedMenuIcon.onclick = () => {
-        // 함수 호출
-        btnMotion(event.currentTarget);
+        btnMotion(event.currentTarget) // 함수호출~
     };
-
-    // 함수 선언
     function btnMotion(x) {
         // 햄버거 버튼: 트리플바 -> x로 바뀜
         // 또 누르면 x -> 트리플바
@@ -20,6 +17,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
         // nav 태그에 change가 추가
         nav.classList.toggle("change");
+
     }
 
 });
