c7cedae18d080daba6bc2b99d9ae55b1fcb6d51d
heejin55 <sandmute@naver.com>
2024-08-22 17:24:27 +0900

    ㅐ
    ㅐ

--- 웹디자인기능사/joy/script/script.js
+++ 웹디자인기능사/joy/script/script.js
@@ -1,4 +1,4 @@
-(() => {
+$(() => {
     // 내비게이션
     const nav = $('.gnb>ul')
     const sub = $('.sub>li>a')
@@ -8,24 +8,24 @@
 
     setInterval(() => {
         $('.slide li a:first-child').fadeOut(1000).next('a').fadeIn(1000);
-    })
+    });
     
 
     // 탭
-    const btn1 = $('.tabs>li').eq(0);
-    const btn2 = $('.tabs>li').eq(1);
+    const btn1 = $('.tabs>li>a').eq(0);
+    const btn2 = $('.tabs>li>a').eq(1);
 
-    const tab1 = $('#tab1>table');
-    const tab2 = $('#tab2>ul');
+    const tab1 = $('.tabs>#tab1>table');
+    const tab2 = $('.tabs>#tab2>ul');
     
 
     btn1.click(() => {
-        tab1.css({display: 'block'});
-        tab2.css({display: 'none'});
+        tab1.addClass('on');
+        tab2.removeClass('on');
     });
     btn2.click(() => {
-        tab2.css({display: 'block'});
-        tab1.css({display: 'none'});
+        tab2.addclass('on');
+        tab1.removeClass('on');
     });
 
 
