0994066d9bb49457355a875139b8828254160a78
heejin55 <sandmute@naver.com>
2024-09-19 01:54:41 +0900

    f
    f

--- 70_pf/br_ver2/js/mdata.js
+++ 70_pf/br_ver2/js/mdata.js
@@ -5,7 +5,7 @@ const mdata = {
         mtit: ["CJ", "cjenm"],
         stit: ["", "Featured Contents", "Newsroom", "Contact Us"],
         s2: {
-            img: ["images/Cont1.jpg","images/Cont2.jpg","images/Cont3.jpg","images/Cont4.jpg","images/Cont5.jpg","images/Cont6.jpg.jpg","images/Cont7.jpg","images/Cont8.jpg"],
+            img: ["images/Cont4.jpg","images/Cont3.jpg","images/Cont2.jpg","images/Cont1.jpg","images/Cont8.jpg","images/Cont7.jpg","images/Cont6.jpg","images/Cont5.jpg"],
             tit: ["Thank you", "Let's carry Sunjae and run", "It's Seojin2", "I-LAND N/a", "I dream openly of Cinderella", "Girls' High School's mystery class 3", "Escape: PROJECT SILENCE", "Backpacker 2"]
         }  
     },
@@ -15,7 +15,7 @@ const mdata = {
         mtit: ["씨제이", "씨제이이엔엠"],
         stit: ["","콘텐츠", "뉴스룸", "고객센터"],
         s2: {
-            img: ["images/Cont1.jpg","images/Cont2.jpg","images/Cont3.jpg","images/Cont4.jpg","images/Cont5.jpg","images/Cont6.jpg.jpg","images/Cont7.jpg","images/Cont8.jpg"],
+            img: ["images/Cont1.jpg","images/Cont2.jpg","images/Cont3.jpg","images/Cont4.jpg","images/Cont5.jpg","images/Cont6.jpg","images/Cont7.jpg","images/Cont8.jpg"],
             tit: ["감사합니다", "선재 업고 튀어", "서진이네2", "I-LAND N/a", "나는 대놓고 신데렐라를 꿈꾼다", "여고 추리반3", "탈출: PROJECT SILENCE", "백패커2"]
         }  
     },
