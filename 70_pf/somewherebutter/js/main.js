5d990f939573a7ee7342c2edd65dd9ecf449755d
heejin55 <sandmute@naver.com>
2024-10-25 22:32:16 +0900

    d
    d

--- 70_pf/somewherebutter/js/main.js
+++ 70_pf/somewherebutter/js/main.js
@@ -2,9 +2,9 @@
 $(() => {
   $('.new_butter_item').bxSlider({
     minSlides: 5,
-    maxSlides: 7,
+    maxSlides: 6,
     slideWidth: 243,
-    slideMargin: 11,
+    slideMargin: 25,
     moveSlides: 1,
     pager: false
   });
